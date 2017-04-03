40a20b1f704e36513e872d125371751ba8e0b38b
Hoang Nam <namhoang.td09@gmail.com>
2017-04-03 09:10:56 +0100

    update components

--- routes/products.js
+++ routes/products.js
@@ -3,150 +3,151 @@
 const Boom = require('boom');
 
 
-exports.register = function (server, options, next) {
+exports.register = function(server, options, next) {
 
-    const db = server.app.db;
+  const db = server.app.db;
 
-    server.route({
-        method: 'GET',
-        path: '/products',
-        handler: function (request, reply) {
+  server.route({
+    method: 'GET',
+    path: '/products',
+    handler: function(request, reply) {
 
-            db.products.find((err, docs) => {
+      db.products.find((err, docs) => {
 
-                if (err) {
-                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                }
-
-                reply(docs);
-            });
-
-        }
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/products',
-        handler: function (request, reply) {
-            var product = request.payload
-
-            db.products.insert(product, (err, doc) => {
-                if (err) {
-                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                }
-
-                reply("Insert successfully")
-            });
-
-        }
-    });
-
-
-    server.route({
-        method: 'GET',
-        path: '/products/{id}',
-        handler: function (request, reply) {
-
-            db.products.findOne({
-                'product_id': request.params.id
-            }, (err, doc) => {
-                console.log(doc);
-                if (err) {
-                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                }
-
-                if (!doc) {
-                    return reply(Boom.notFound());
-                }
-
-                reply(doc);
-            });
-
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
         }
 
-     });
-
-    server.route({
-        method: 'PUT',
-        path: '/products/{id}',
-        handler: function (request, reply) {
-
-            db.products.update({
-                'product_id': request.params.id
-            }, {
-                $set: request.payload
-            }, (err, doc) => {
-
-                if (err) {
-                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                }
-
-                if (!doc) {
-                    return reply(Boom.notFound());
-                }
-
-                reply(doc);
-            });
-
-        }
-
-     });
-
-
-     server.route({
-         method: 'POST',
-         path: '/products/{id}',
-         handler: function (request, reply) {
-
-             db.products.update({
-                 'product_id': request.params.id
-             }, (err, doc) => {
-                 console.log(doc);
-                 if (err) {
-                     return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                 }
-
-                 if (!doc) {
-                     return reply(Boom.notFound());
-                 }
-
-                 reply(doc);
-             });
-
-         }
-
+        reply(docs);
       });
 
+    }
+  });
 
-     server.route({
-        method: 'DELETE',
-        path: '/products/{id}',
-        handler: function (request, reply) {
+  server.route({
+    method: 'POST',
+    path: '/products',
+    handler: function(request, reply) {
+      var product = request.payload
 
-            db.products.remove({
-                'product_id': request.params.id
-            }, function (err, result) {
-                if (err) {
-                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                }
+      db.products.insert(product, (err, doc) => {
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
 
-                if (result.n === 0) {
-                    return reply(Boom.notFound());
-                }
-
-                reply().code(204);
-            });
-        },
-        // config: {
-        //     auth: 'jwt'
-        // }
+        reply("Insert successfully")
       });
 
+    }
+  });
 
 
-    return next();
+  server.route({
+    method: 'GET',
+    path: '/products/{id}',
+    handler: function(request, reply) {
+
+      db.products.findOne({
+        'product_id': request.params.id
+      }, (err, doc) => {
+        // console.log(doc);
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+
+        if (!doc) {
+          return reply(Boom.notFound());
+        }
+
+        reply(doc);
+      });
+
+    }
+
+  });
+
+  server.route({
+    method: 'PUT',
+    path: '/products/{id}',
+    handler: function(request, reply) {
+
+      console.log(request.payload);
+      db.products.update({
+        'product_id': request.params.id
+      }, {
+        $set: request.payload
+      }, (err, doc) => {
+
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+
+        if (!doc) {
+          return reply(Boom.notFound());
+        }
+
+        reply(doc);
+      });
+
+    }
+
+  });
+
+
+  server.route({
+    method: 'POST',
+    path: '/products/{id}',
+    handler: function(request, reply) {
+
+      db.products.update({
+        'product_id': request.params.id
+      }, (err, doc) => {
+        console.log(doc);
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+
+        if (!doc) {
+          return reply(Boom.notFound());
+        }
+
+        reply(doc);
+      });
+
+    }
+
+  });
+
+
+  server.route({
+    method: 'DELETE',
+    path: '/products/{id}',
+    handler: function(request, reply) {
+
+      db.products.remove({
+        'product_id': request.params.id
+      }, function(err, result) {
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+
+        if (result.n === 0) {
+          return reply(Boom.notFound());
+        }
+
+        reply().code(204);
+      });
+    },
+    // config: {
+    //     auth: 'jwt'
+    // }
+  });
+
+
+
+  return next();
 };
 
 exports.register.attributes = {
-    name: 'routes-products'
+  name: 'routes-products'
 };
--- routes/users.js
+++ routes/users.js
@@ -1,75 +1,125 @@
 'use strict';
 
 const Boom = require('boom');
-const jwt  = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 var config = require('./../config');
 
-exports.register = function (server, options, next) {
+exports.register = function(server, options, next) {
 
-    const db = server.app.db;
+  const db = server.app.db;
 
-    server.route({
-        method: 'GET',
-        path: '/users',
-        handler: function (request, reply) {
+  server.route({
+    method: 'GET',
+    path: '/users',
+    handler: function(request, reply) {
 
-            db.users.find((err, docs) => {
+      db.users.find((err, docs) => {
 
-                if (err) {
-                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
-                }
-
-                reply(docs);
-            });
-
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
         }
-    });
+
+        reply(docs);
+      });
+
+    }
+  });
+
+  server.route({
+    method: 'DELETE',
+    path: '/users/{id}',
+    handler: function(request, reply) {
+      console.log(request.params.id);
+
+      db.users.remove({
+        'user_id': request.params.id
+      }, function(err, result) {
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+
+        if (result.n === 0) {
+          return reply(Boom.notFound());
+        }
+
+        reply().code(204);
+      });
+
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/users/{id}',
+    handler: function(request, reply) {
+      console.log(request.params.id);
+
+      db.users.find({
+        'user_id': request.params.id
+      }, function(err, result) {
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+
+        if (result.n === 0) {
+          return reply(Boom.notFound());
+        }
+
+        reply(result);
+      });
+
+    }
+  });
 
 
-    server.route({
-        method: 'POST',
-        path: '/authenticate',
-        handler: function (request, reply) {
-          db.users.findOne({
-            username: request.payload.name,
-          }, (err, user) => {
 
-              if (err) {
-                  return reply(Boom.wrap(err, 'Internal MongoDB error'));
-              }
+  server.route({
+    method: 'POST',
+    path: '/authenticate',
+    handler: function(request, reply) {
+      db.users.findOne({
+        username: request.payload.name,
+      }, (err, user) => {
 
-              if(!user) {
-                reply({ success: false, message: 'Authentication failed. User not found.' });
-              }
+        if (err) {
+          return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
 
-              else if (user.password != request.payload.password) {
-                reply({ success: false, message: 'Authentication failed. Wrong password.' });
-              }
-              else {
-                // if user is found and password is right
-                // create a token
-                var token = jwt.sign(user, config.secret, {
-                  expiresIn: 86400 // expires in 24 hours
-                });
-
-                // return the information including token as JSON
-                reply({
-                  success: true,
-                  message: 'Enjoy your token!',
-                  token: token
-                });
-              }
-
+        if (!user) {
+          reply({
+            success: false,
+            message: 'Authentication failed. User not found.'
+          });
+        } else if (user.password != request.payload.password) {
+          reply({
+            success: false,
+            message: 'Authentication failed. Wrong password.'
+          });
+        } else {
+          // if user is found and password is right
+          // create a token
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
           });
 
-
+          // return the information including token as JSON
+          reply({
+            success: true,
+            message: 'Enjoy your token!',
+            token: token
+          });
         }
-    });
+
+      });
 
 
-    return next();
+    }
+  });
+
+
+  return next();
 };
 
 exports.register.attributes = {
-    name: 'routes-users'
+  name: 'routes-users'
 };
